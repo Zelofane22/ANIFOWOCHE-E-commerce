c29e175ef739e3f2f73d90308b9e6e938a056a31
Zelofane22 <fouadechitou@gmail.com>
2026-08-01 16:01:52 +0200

    refactor(tests): streamline product filtering test and remove unused code

--- code/backend/apps/tests_debug2.py
+++ code/backend/apps/tests_debug2.py
@@ -1,5 +1,7 @@
 from rest_framework.test import APITestCase
 from apps.core.factories import CategoryFactory, ProductFactory
+from apps.products.filters import ProductFilter
+from apps.products.models import Product
 from apps.products.views import ProductViewSet
 
 
@@ -11,13 +13,7 @@ class DebugViewSet(APITestCase):
         ProductFactory(category=restauration, name="Crepes", slug="crepes", price_xof=1500, stock=0, made_to_order=True)
         ProductFactory(category=self.category, name="Pagne", slug="pagne", price_xof=5000, stock=10)
 
-        from django_filters.rest_framework import DjangoFilterBackend
-        view = ProductViewSet()
-        view.action = 'list'
-        view.setup(self.client.get('/api/products/', {'in_stock': '1'}).wsgi_request)
-        backend = DjangoFilterBackend()
-        fs = backend.get_filterset(view.request, ProductViewSet.queryset, view)
-        print("filterset class:", type(fs).__name__ if fs else None)
-        print("form data:", fs.form.data if fs else None)
-        print("errors:", fs.form.errors if fs else None)
-        print("qs:", list(fs.qs.values_list('slug', 'stock', 'made_to_order')) if fs else None)
+        print("query_params on request:", self.client.get("/api/products/", {"in_stock": "1"}).wsgi_request.GET)
+        fs = ProductFilter({"in_stock": "1"}, queryset=ProductViewSet.queryset)
+        print("valid:", fs.is_valid(), "errors:", fs.form.errors)
+        print("direct qs:", list(fs.qs.values_list("slug", "stock", "made_to_order")))
